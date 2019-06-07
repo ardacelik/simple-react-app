c1e77c74e7990869575ba9fadcc6c9fa8c5bdd1e
Arda Celik <arda.celik@ymail.com>
2019-06-06 21:03:21 -0400

    First commit: react application is initialized using create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="row align-items-center">
+            <h1 className="">Hello World!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
