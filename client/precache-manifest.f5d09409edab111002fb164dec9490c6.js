bd416c6a7bd51906d004048072f017df01550e1e
Arda Celik <arda.celik@ymail.com>
2019-06-20 10:32:54 -0400

    The back-end and the front-end are integrated.

---
+++ client/precache-manifest.f5d09409edab111002fb164dec9490c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa27acb713f16a2268168ac63a5ca6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cc00b5520815909fb41",
+    "url": "/static/css/main.506e1245.chunk.css"
+  },
+  {
+    "revision": "dc859fa693e8130d3c07",
+    "url": "/static/js/2.bf880dac.chunk.js"
+  },
+  {
+    "revision": "5cc00b5520815909fb41",
+    "url": "/static/js/main.7288ed6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
